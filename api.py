79c6a78f29dd8c62ee0bdd326e3c92cb7fce0776
Rajani-07 <drajani3379@gmail.com>
2025-10-29 12:51:05 +0530

    Update api.py

--- api.py
+++ api.py
@@ -26,14 +26,14 @@ app = FastAPI(
     redoc_url="/redoc"
 )
 
-origins = [
+'''origins = [
     "http://localhost:3000",  # Allow requests from this origin
-]
+]'''
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
